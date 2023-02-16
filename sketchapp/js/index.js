abc7dc83b86837466549fd529b7e9d08a29bad4c
DariaG16 <dariagolitsyna16@gmail.com>
2023-02-16 14:01:37 -0500

    sobbing

--- sketchapp/js/index.js
+++ sketchapp/js/index.js
@@ -1,16 +1,27 @@
 const DOMSelectors = {
   saveClr: document.querySelector("#saveColor"),
   savedClrs: document.querySelector("#saved"),
+  color: document.querySelector("#color"),
 };
+function works() {
+  console.log(``);
+}
+DOMSelectors.saveClr.addEventListener("click", works());
+let colorPick = DOMSelectors.color.value;
+
 DOMSelectors.saveClr.addEventListener("click", (i) => {
-  i === 0;
+  i = 0;
   if (i < 16) {
     DOMSelectors.savedClrs.insertAdjacentHTML(
       "beforeend",
       `
-    <button id="colors"></button>`
+      
+    <button style="background-color:${colorPick};" id="colors"></button>`
     );
     i++;
+    console.log("u did it");
+  } else {
+    console.log("you suck");
   }
 });
 let mouseX = null;
@@ -22,18 +33,15 @@ ctx.lineWidth = 5; //ctx.lineWidth = ${the number from the slider}
 canvas.width = 1500;
 canvas.height = 948;
 
-canvas.addEventListener("mousemove", (e) => {
+canvas.addEventListener("click", (e) => {
   if (mouseX == null || mouseY == null) {
-    mouseX = e.clientX -= 400;
-    mouseY = e.clientY;
+    mouseX = e.clientX -= 406;
+    mouseY = e.clientY += 7;
     return;
   }
-
-  console.log("MouseX: " + e.clientX);
-  console.log("MouseY: " + e.clientY);
   // Current mouse position
-  let mouseX2 = (e.clientX -= 400);
-  let mouseY2 = e.clientY;
+  let mouseX2 = (e.clientX -= 406);
+  let mouseY2 = (e.clientY -= 7);
 
   // Drawing a line from the previous mouse position to the current mouse position
   ctx.beginPath();
