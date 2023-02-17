94b78b3d778578382eb9ee38c5d5918b2a5cac6c
DariaG16 <dariagolitsyna16@gmail.com>
2023-02-17 13:59:56 -0500

    yippeeee

--- sketchapp/js/index.js
+++ sketchapp/js/index.js
@@ -2,25 +2,24 @@ const DOMSelectors = {
   saveClr: document.querySelector("#saveColor"),
   savedClrs: document.querySelector("#saved"),
   color: document.querySelector("#color"),
+  size: document.querySelector("#size"),
+  lblSize: document.querySelector("#brushSize"),
 };
-
-DOMSelectors.saveClr.addEventListener("input", function (e) {
-  console.log("hi");
-});
-function works() {
-  console.log("smh");
-}
-let colorPick = "#000000";
-DOMSelectors.color.addEventListener(
-  "input",
-  () => {
-    colorPick = DOMSelectors.color.value;
-    console.log(colorPick);
-  },
-  false
-);
-
 let amt = 0;
+let brSize = 1;
+let mouseX = null;
+let mouseY = null;
+
+DOMSelectors.color.addEventListener("input", () => input());
+DOMSelectors.size.addEventListener("input", () => input());
+
+function input() {
+  colorPick = DOMSelectors.color.value;
+  brSize = DOMSelectors.size.value;
+  console.log(brSize);
+  DOMSelectors.lblSize.innerHTML = `Brush Size: ${brSize} px`;
+}
+
 DOMSelectors.saveClr.addEventListener("click", () => {
   if (amt < 15) {
     DOMSelectors.savedClrs.insertAdjacentHTML(
@@ -33,12 +32,9 @@ DOMSelectors.saveClr.addEventListener("click", () => {
     console.log("you suck");
   }
 });
-let mouseX = null;
-let mouseY = null;
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-ctx.lineWidth = 5; //ctx.lineWidth = ${the number from the slider}
-
 canvas.width = 1500;
 canvas.height = 948;
 
@@ -48,17 +44,19 @@ canvas.addEventListener("click", (e) => {
     mouseY = e.clientY += 7;
     return;
   }
-  // Current mouse position
   let mouseX2 = (e.clientX -= 406);
   let mouseY2 = (e.clientY -= 7);
-
-  // Drawing a line from the previous mouse position to the current mouse position
   ctx.beginPath();
+  ctx.lineWidth = brSize;
+  ctx.strokeStyle = colorPick;
   ctx.moveTo(mouseX2, mouseY2);
   ctx.lineTo(mouseX, mouseY);
   ctx.stroke();
-
   // Update previous mouse position
   mouseX = mouseX2;
   mouseY = mouseY2;
 });
+
+//If user clicks shift, make lines (the funcion i currently have)
+//make the lines smooth
+//create brush library to choose from
