567d76a234419de45c111df42dbcde6b3953af7d
DariaG16 <113132247+DariaG16@users.noreply.github.com>
2023-03-04 22:28:07 -0500

    3/4

--- sketchapp/js/index.js
+++ sketchapp/js/index.js
@@ -1,5 +1,4 @@
 import "../styles/styles.css";
-
 const DOMSelectors = {
   saveClr: document.querySelector("#saveColor"),
   savedClrs: document.querySelector("#saved"),
@@ -16,40 +15,59 @@ let mouseY = null;
 let amt = 0;
 let brSize = 1;
 let makeLine = false;
-let colorPick;
-
+let colorPick = "#000000";
+let savedClrsArray;
 DOMSelectors.color.addEventListener("input", () => input());
 DOMSelectors.size.addEventListener("input", () => input());
 
 function input() {
+  //checks the values for the inputs i have when the function detects an input
   colorPick = DOMSelectors.color.value;
   brSize = DOMSelectors.size.value;
   DOMSelectors.lblSize.innerHTML = `Brush Size: ${brSize} px`;
 }
+
 DOMSelectors.erase.addEventListener("click", () => {
+  //erase button
   colorPick = "#FFFFFF";
 });
-let saveColors = ["#000000"];
 
+let saveColors = new Set();
 DOMSelectors.saveClr.addEventListener("click", () => {
   if (amt < 15) {
     DOMSelectors.savedClrs.insertAdjacentHTML(
       "beforeend",
-      `<button style="background-color:${colorPick};" class="colors"></button>`
+      `<button style="background-color:${colorPick};" class="colors" id="${colorPick}"></button>` //saves the colors into an array and into buttons
     );
-    saveColors.push(colorPick);
-    let lastSaved = saveColors[saveColors.length - 1];
     amt++;
-    console.log(saveColors);
-    console.log(lastSaved);
+  } else if (amt === 15) {
+    //im probably gonna delete this, too complicated i believe. But it makes a select option after 15 colors have been saved for the opportunity to save more colors, and adds options... its kinda broken tho
+    DOMSelectors.savedClrs.insertAdjacentHTML(
+      "beforeend",
+      `<label for="Saved Colors">More colors:</label>
+      <select name="Saved Colors" id="clrSavedArray"> Saved Colors</select>`
+    );
+    savedClrsArray = document.querySelector("#clrSavedArray");
+    savedClrsArray.insertAdjacentHTML(
+      "beforeend",
+      `<option style="background-color:${colorPick};" class="color" id="${colorPick}"></option>`
+    );
   } else {
-    console.log("you suck");
+    savedClrsArray.insertAdjacentHTML(
+      "beforeend",
+      `<option style="background-color:${colorPick};" class="color" id="${colorPick}"></option>`
+    );
   }
+  saveColors.add(colorPick);
+  console.log(saveColors);
 });
-const color = document.querySelector("colors");
 
-DOMSelectors.savedClrs.addEventListener("click", () => {
-  console.log(color);
+DOMSelectors.savedClrs.innerHTML = ""; //not really sure what this does
+saveColors.forEach((color) => {
+  document.querySelector(`#${colorPick}`).addEventListener("click", () => {
+    console.log(color.style.backgroundColor);
+    colorPick = color;
+  }); // deleted the inserthtml thing cause i dont get why i'd need to make new html for each color??? idk i dont understand
 });
 
 const draw = (e) => {
@@ -63,12 +81,11 @@ const draw = (e) => {
   let mouseY2 = e.clientY;
   ctx.lineTo(mouseX2, mouseY2);
   ctx.stroke();
-  // Update previous mouse position
-};
+}; // i know i have to fix this bogdan its just complicated so im procrastinating
 
 canvas.addEventListener("mousedown", (e) => {
   makeLine = true;
-  mouseX = e.clientX; //figure this out
+  mouseX = e.clientX;
   mouseY = e.clientY;
 });
 canvas.addEventListener("mouseup", (e) => {
@@ -77,6 +94,3 @@ canvas.addEventListener("mouseup", (e) => {
   ctx.beginPath();
 });
 canvas.addEventListener("mousemove", draw);
-//If user clicks shift, make lines (the funcion i currently have)
-//make the lines smooth
-//create brush library to choose from
